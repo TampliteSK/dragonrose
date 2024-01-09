e1f54b1c316bb24be149637e94bd6936adbfce59
Tamp <regentfoo.rf@gmail.com>
2024-01-09 20:58:29 +0800

    CMake Secretly Hates Me

---
+++ CMakeFiles/Dragonrose.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/attack.c" "CMakeFiles/Dragonrose.dir/Source/Engine/attack.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/attack.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/bitboards.c" "CMakeFiles/Dragonrose.dir/Source/Engine/bitboards.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/bitboards.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/board.c" "CMakeFiles/Dragonrose.dir/Source/Engine/board.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/board.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/data.c" "CMakeFiles/Dragonrose.dir/Source/Engine/data.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/data.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/evaluate.c" "CMakeFiles/Dragonrose.dir/Source/Engine/evaluate.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/evaluate.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/hashkeys.c" "CMakeFiles/Dragonrose.dir/Source/Engine/hashkeys.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/hashkeys.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/init.c" "CMakeFiles/Dragonrose.dir/Source/Engine/init.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/init.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/io.c" "CMakeFiles/Dragonrose.dir/Source/Engine/io.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/io.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/main.c" "CMakeFiles/Dragonrose.dir/Source/Engine/main.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/main.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/makemove.c" "CMakeFiles/Dragonrose.dir/Source/Engine/makemove.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/makemove.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/misc.c" "CMakeFiles/Dragonrose.dir/Source/Engine/misc.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/misc.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/movegen.c" "CMakeFiles/Dragonrose.dir/Source/Engine/movegen.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/movegen.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/perft.c" "CMakeFiles/Dragonrose.dir/Source/Engine/perft.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/perft.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/polybook.c" "CMakeFiles/Dragonrose.dir/Source/Engine/polybook.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/polybook.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/polykeys.c" "CMakeFiles/Dragonrose.dir/Source/Engine/polykeys.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/polykeys.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/pvtable.c" "CMakeFiles/Dragonrose.dir/Source/Engine/pvtable.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/pvtable.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/search.c" "CMakeFiles/Dragonrose.dir/Source/Engine/search.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/search.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/uci.c" "CMakeFiles/Dragonrose.dir/Source/Engine/uci.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/uci.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/validate.c" "CMakeFiles/Dragonrose.dir/Source/Engine/validate.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/validate.c.obj.d"
+  "C:/Users/regen/Documents/Files/Programming/C/Dragonrose/Source/Engine/xboard.c" "CMakeFiles/Dragonrose.dir/Source/Engine/xboard.c.obj" "gcc" "CMakeFiles/Dragonrose.dir/Source/Engine/xboard.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
